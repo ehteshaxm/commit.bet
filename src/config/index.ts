c3c76bcd534569bf01e1b871baf51f98c3244cfc
Ehtesham <siddiquie.ehtesham@gmail.com>
2024-08-11 16:58:56 +0530

    fix: config route

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const DOMAIN =
   process.env.NODE_ENV === "development"
-    ? "https://b4a7-2401-4900-57e3-abae-3d1b-b88b-b84a-f553.ngrok-free.app"
+    ? "http://localhost:3000"
     : process.env.NEXT_PUBLIC_DOMAIN
     ? `https://${process.env.NEXT_PUBLIC_DOMAIN}`
     : "https://commit-bet.vercel.app";
